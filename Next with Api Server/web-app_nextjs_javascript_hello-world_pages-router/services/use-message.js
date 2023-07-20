46bc6fa3f0fb0b878960201cce079c98db67c7dc
talha.rizwan <talha.rizwan7170@hotmail.com>
2023-07-20 16:51:47 +0500

    AuthO Next + Springboot

---
+++ Next with Api Server/web-app_nextjs_javascript_hello-world_pages-router/services/use-message.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export const useMessage = (config) => {
+  const [message, setMessage] = useState("");
+
+  const fetcher = async (config) => await axios(config).then((res) => res.data);
+
+  const {
+    data,
+    error,
+    isLoading: isApiResponseLoading,
+  } = useSWR(config, fetcher);
+
+  useEffect(() => {
+    if (isApiResponseLoading) {
+      return;
+    }
+
+    if (error) {
+      setMessage(
+        error.response && error.response.data
+          ? JSON.stringify(error.response.data, null, 2)
+          : "Something went wrong"
+      );
+    }
+
+    if (data) {
+      setMessage(JSON.stringify(data, null, 2));
+    }
+  }, [data, error, isApiResponseLoading]);
+  console.log(message);
+
+  return { message };
+};
